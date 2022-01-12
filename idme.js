5ab516433a5628132c8b2a433a3e96db50d360c5
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2022-01-12 18:29:52 -0500

    Added comments

--- idme.js
+++ idme.js
@@ -1,14 +1,15 @@
+// Create a function, define a variable IDme. Open in a new window. 
 $(function() {
     var IDme = {
       access_token: window.location.hash.split("&")[0].match(/[^#access_token=]\w+/)[0],
-  
+// Create a function params, return the api and access token.   
       params: function() {
         return {
           url: "https://api.id.me/api/public/v3/attributes.json?access_token=" + this.access_token,
           dataType: "jsonp"
         }
       },
-  
+// Create a function request, using access token and parameters, then verified. Display message. 
       request: function() {
         if (this.access_token) {
           $.get(this.params()).done(function(payload) {
@@ -20,7 +21,7 @@ $(function() {
         }
       }
     };
-  
+// Execute request function.   
     IDme.request();
   });
   
