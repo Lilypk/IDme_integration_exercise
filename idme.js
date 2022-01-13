ca865364e1f2c458cc771cadb4f1553166d33922
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2022-01-13 18:10:18 -0500

    Added some more comments in idme.js

--- idme.js
+++ idme.js
@@ -2,14 +2,14 @@
 $(function() {
     var IDme = {
       access_token: window.location.hash.split("&")[0].match(/[^#access_token=]\w+/)[0],
-// Create a function params, return the api and access token.   
+// Create a function called params, make a request to IDme API, return in jsonp format. 
       params: function() {
         return {
           url: "https://api.id.me/api/public/v3/attributes.json?access_token=" + this.access_token,
           dataType: "jsonp"
         }
       },
-// Create a function request, using access token and parameters, then verified. Display message. 
+// Create a function called request, display user's data and status. 
       request: function() {
         if (this.access_token) {
           $.get(this.params()).done(function(payload) {
